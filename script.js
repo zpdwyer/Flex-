6e00f7e338f29058bca72b844e3de82f0329114b
zpdwyer <zpdwyer@iastate.edu>
2015-08-05 16:20:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ window.onload = function load_sound() {
            var count=0;
            var winner;
            var x=0;
+           var userRef = new Firebase("https://flex.firebaseio.com/users");
+           var authData = userRef.getAuth();
+           var userID;
+           var votesLeftRef;
            $(set.tracks).each(function(i, track) {
                               track_IDs.push(track.id);
                               });
@@ -26,7 +30,7 @@ window.onload = function load_sound() {
            var Song_Choices = [];
            var is_playing = false;
            var nextSongs = [];
-           
+           max = track_IDs.length;
            if(firstPlay!==false)
            {
            
@@ -37,21 +41,26 @@ window.onload = function load_sound() {
 											Song_Choices.push(track_IDs[num]);
                                             count++;
            }
-           load_Songs(Song_Choices);
            firstPlay=false;
+           Song_Choices = load_Songs(Song_Choices, max, track_IDs);
            count=0;
            }
            while(count<=track_IDs.length)
            {
-           nextSongs.push(track_IDs[Song_Choices.pop()]);
-           nextSongs.push(track_IDs[Song_Choices.pop()]);
-           nextSongs.push(track_IDs[Song_Choices.pop()]);
-           load_Songs(nextSongs);
            
-           SC.stream('/tracks/' + track_ID, {volume:50, onfinish:is_playing=false}, playSong(sound));
+           if(firstPlay===false)
+           {
+           nextSongs.push(track_IDs[Song_Choices[0]]);
+           nextSongs.push(track_IDs[Song_Choices[1]]);
+           nextSongs.push(track_IDs[Song_Choices[2]]);
+           load_Songs(nextSongs, max, track_IDs);
+           }
            
-           var userID = authData.uniqueID;
-           var votesLeftRef = userRef.child(userID + '/votesLeft');
+           playSong(track_ID);
+           
+           
+           userID = authData.uniqueID;
+           votesLeftRef = userRef.child(userID + '/votesLeft');
            votesLeftRef.set(1);
            
            winner = voteCount();
@@ -66,16 +75,16 @@ window.onload = function load_sound() {
 											num = Math.random() * (max - 0) + 0;
 											Song_Choices.push(track_IDs.getValue(num));
            }
-           load_Songs(Song_Choices);
+           load_Songs(Song_Choices, max, track_IDs);
            }
-           
+           count++;
            }
            
            }
            
            );
 };
-function load_Songs(songChoices){
+/*function load_Songs(songChoices){
     SC.initialize({
                   client_id: 'e37f3c1f5f6ba94620a84468518dc09d'
                   });
@@ -83,24 +92,27 @@ function load_Songs(songChoices){
     var choice1 = songChoices[0];
     var choice2 = songChoices[1];
     var choice3 = songChoices[2];
-    var i=0;
-    var songTitles=[];
-    while(i<songChoices.length)
-    {
-        SC.get('/tracks/' + choice1, songTitles.push(track.title));
-    }
-    document.getElementById("Choice1").innerHTML = songTitles[0];
-    document.getElementById("Choice2").innerHTML = songTitles[1];
-}
+    
+        SC.get('/tracks/' + choice1, function(track){
+               document.getElementById("Choice1").innerHTML = track.title;
+        });
+    
+    SC.get('/tracks/' + choice2, function(track){
+           console.log(track.title);
+           document.getElementById("Choice2").innerHTML = track.title;
+           });
+}*/
 
 
-function playSong(sound)
+function playSong(track_ID)
 {
     is_playing = true;
+    this.track_ID=track_ID;
+    SC.stream('/tracks/' + track_ID, {volume:00, onfinish:is_playing=false}, function(sound){
     sound.play();
-    var userRef = new Firebase("https://<your-firebase-app>.firebaseio.com/users");
-    var authData = ref.getAuth();
-    var votingRef = new Firebase("https://<your-firebase-app>.firebaseio.com/songChoices");
+    var userRef = new Firebase("https://flex.firebaseio.com/users");
+    var authData = userRef.getAuth();
+    var votingRef = new Firebase("https://flex.firebaseio.com/songChoices");
     choiceOneRef = votingRef.child('/Choice_1');
     choiceOneRef.set(0);
     choiceTwoRef = votingRef.child('/Choice_2');
@@ -141,11 +153,12 @@ function playSong(sound)
             };
         }
     }
+              });
     
 }
 
 function voteCount(){
-    var votingRef = new Firebase("https://<your-firebase-app>.firebaseio.com/songChoices");
+    var votingRef = new Firebase("https://flex.firebaseio.com/songChoices");
     var count=0;
     choiceOneRef = votingRef.child('/Choice_1');
     choiceTwoRef = votingRef.child('/Choice_2');
@@ -168,90 +181,67 @@ function voteCount(){
     
 
 }
-/*function Vote_Count(maxRefOne, maxRefTwo, maxRefRandom){
- this.maxRefOne = maxRefOne;
- this.maxRefTwo = maxRefTwo;
- this.maxRefRandom = maxRefRandom;
- var voteRef = new Firebase("https://<your-firebase-app>.firebaseio.com/songChoices");
- var currentParent = maxRefOne.parent();
- var countOne=0;
- var countTwo=0;
- var countRandom=0;
- var winnerValue;
- 
- while(currentParent!==0)
- {
- maxRefOne.remove();
- countOne++;
- currentParent = currentParent.parent();
- }
- 
- currentParent = maxRefTwo.parent();
- while(currentParent!==0)
- {
- maxRefTwo.remove();
- countTwo++;
- currentParent = currentParent.parent();
- }
- currentParent = maxRefRandom.parent();
- while(currentParent!==0)
- {
- maxRefRandom.remove();
- countRandom++;
- currentParent = currentParent.parent();
- }
- if(countOne > countTwo)
- {
- winnerValue = 1;
- if(countOne>countRandom)
- {
- return winnerValue;
- }
- else
- {
- winnerValue = 3;
- return winnerValue;
- }
- }
- else
- {
- winnerVlaue = 2;
- if(countTwo > countRandom)
- {
- return winnerValue;
- }
- else
- {
- winnerValue = 3;
- return winnerValue;
- }
- }
- 
- }*/
-/*function Voting_Period(){
- 
- var userRef = new Firebase("https://<your-firebase-app>.firebaseio.com/users");
- var authData = ref.getAuth();
- var votingRef = new Firebase("https://<your-firebase-app>.firebaseio.com/songChoices");
- 
- if(authData){
- var userID = authData.uniqueID;
- var votesLeftRef = userRef.child(userID + '/votesLeft');
- var votesLeft = votesLeftRef.val();
- if(votesLeft!=0) {
- if(document.getElementById("Choice1").onclick(function())){
- var choiceOneRef = votingRef.child('/Choice_1');
- var maxRefOne = choiceOneRef.push(1);
- }
- if(document.getElementById("Choice2").onclick(function())){
- var choiceTwoRef = votingRef.child('/Choice_2');
- var maxRefTwo = choiceOneRef.push(1);
- }
- if(document.getElementById("Random").onclick(function())){
- var choiceRandom = votingRef.child('/Random');
- var maxRefRandom = choiceOneRef.push(1);
- }
- }
- }
- 
- }*/
+
+function load_Songs(songChoices, max, track_IDs){
+    SC.initialize({
+                  client_id: 'e37f3c1f5f6ba94620a84468518dc09d'
+                  });
+    this.songChoices=songChoices;
+    this.max=max;
+    this.track_IDs=track_IDs;
+    var choice1 = songChoices[0];
+    var choice2 = songChoices[1];
+    var choice3 = songChoices[2];
+    var num;
+    
+    var song1 = SC.get('/tracks/' + choice1, function(track){
+           return track.title;
+           });
+    
+    var song2 = SC.get('/tracks/' + choice2, function(track){
+                       
+                       return track.title;
+           });
+    var song3 = SC.get('/tracks/' + choice3, function(track){
+                       return track.title;
+                       });
+    //console.log(song1);
+    console.log("test");
+    while(song1 === 'undefined')
+    {
+        num = Math.floor(Math.random() * (max - 0) + 0);
+        choice1 = track_IDs[num];
+        song1 = SC.get('/tracks/' + choice1, function(track){
+                           return track.title;
+                           });
+        
+    }
+    document.getElementById("Choice1").innerHTML = song1;
+    while(song2 === 'undefined')
+    {
+        num = Math.floor(Math.random() * (max - 0) + 0);
+        choice2 = track_IDs[num];
+        song2 = SC.get('/tracks/' + choice2, function(track){
+                           return track.title;
+                           });
+        
+    }
+    document.getElementById("Choice1").innerHTML = song2;
+    while(song3 === 'undefined')
+    {
+        num = Math.floor(Math.random() * (max - 0) + 0);
+        choice3 = track_IDs[num];
+        song3 = SC.get('/tracks/' + choice3, function(track){
+                           return track.title;
+                           });
+        
+    }
+    
+    songChoices[0]= choice1;
+    songChoices[1]= choice2;
+    songChoices[2]= choice3;
+    
+    return songChoices;
+    
+}
+
